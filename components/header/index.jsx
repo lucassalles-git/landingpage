70bc0b7c11c4546ff796ca3c3137ec2c8432938a
lucassalles <lucasmssp@gmail.com>
2025-05-22 07:42:59 -0300

    Criando setup inicial e component header

---
+++ components/header/index.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image"; //importe da imagem
+import Link from "next/link"; //import dos links
+import Logo from "../../public/images/logo.svg"; //import da logo
+import Styles from "./header.module.scss" //import dos estilos
+
+const Header = () => {
+    return (
+        // Chamando os estilos
+        <div className={Styles.container}>
+            <div className={Styles.logotipo}>
+                <Image src={Logo} />
+            </div>
+            <nav className={Styles.menu}>
+                <Link href="/">Home</Link>
+                <Link href="/">O que fazemos</Link>
+                <Link href="/">Cases</Link>
+            </nav>
+            <div className={Styles.action}>
+                <button className={Styles.button}>Fale conosco</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
