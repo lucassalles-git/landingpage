baf934cd72207465647fe75bdbaf004348249610
lucassalles <lucasmssp@gmail.com>
2025-05-23 07:00:48 -0300

    Criando variação do button

---
+++ components/button/index.jsx
@@ -0,0 +1,19 @@
+import Styles from "./button.module.scss"
+
+// Props 
+// title: String
+// Kind:  "primary" | "secundary"
+
+const Button = ({title, kind}) => {
+    const generationClassByKind = () => {
+        if (kind === "secundary") return Styles.secundary;
+
+        return Styles.primary;
+    }
+
+    return (
+        <button className={`${Styles.button} ${generationClassByKind()}`}>{title}</button>
+    );
+}
+
+export default Button;
